0809c9d3f6d64f612e9adcc97408868fcd79bcb7
renjinwei <renjwjx@gmail.com>
2020-02-06 16:51:02 +0800

    Initial Commit

---
+++ animationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  animationDemo
+//
+//  Created by jinren on 2/6/20.
+//  Copyright © 2020 jinren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ animationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  animationDemo
+//
+//  Created by jinren on 2/6/20.
+//  Copyright © 2020 jinren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
